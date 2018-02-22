8e8432aab010d0d431e936891873c883c0934ea4
Matthew Harris <mharr171.z@gmail.com>
2018-02-22 10:03:03 -0800

    Head created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { Container, Header, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
-import Drink from './components/Drink.jsx';
-import DrinkForm from './components/DrinkForm.jsx';
+import { Container, Dimmer, Loader } from 'semantic-ui-react'
+import Head from './components/Head.jsx';
 
 class App extends Component {
   constructor () {
@@ -9,24 +8,46 @@ class App extends Component {
     this.state = {
       drinks: null,
       drink: null,
-      buttonsDisabled: false
+      editFormIsOpen: false
     }
     this.getDrinks = this.getDrinks.bind(this)
     this.getDrink = this.getDrink.bind(this)
-    this.getDrinksAndGetDrink = this.getDrinksAndGetDrink.bind(this)
-    this.newDrink = this.newDrink.bind(this)
+  }
 
-    this.post = this.post.bind(this)
-    this.delete = this.delete.bind(this)
-    this.patchDrink = this.patchDrink.bind(this)
-    this.postIngredient = this.postIngredient.bind(this)
-    this.deleteIngredient = this.deleteIngredient.bind(this)
-
-    this.flipButtonsDisabled = this.flipButtonsDisabled.bind(this)
+  render () {
+    let {drinks, drink, editFormIsOpen} = this.state
+    return drinks
+    ? <Container text>
+        <Head />
+        <div class="ui hidden divider"></div>
+      </Container>
+    : <Container text>
+        <Dimmer active inverted>
+          <Loader content='Loading' />
+        </Dimmer>
+      </Container>
   }
 
   componentDidMount () {
     this.getDrinks()
+  }
+
+  getDrinks (id = -1) {
+    this.fetch('api/drinks')
+      .then(drinks => {
+        this.setState({drinks: drinks})
+        if (!this.state.drink){
+          this.getDrink(drinks[0].id)
+        }
+        if (id !== -1){
+          this.getDrink(drinks[0].id)
+        }
+      })
+  }
+
+  getDrink (id) {
+    this.fetch(`api/drinks/${id}`)
+      .then(drink => this.setState({drink: drink}))
   }
 
   fetch (endpoint) {
@@ -78,175 +99,6 @@ class App extends Component {
     }
   }
 
-  async deleteIngredient (endpoint, drinkId) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    const options = {
-      method: 'DELETE',
-      headers
-    };
-
-    const request = new Request(endpoint, options);
-    const response = await fetch(request);
-    const status = await response.status;
-
-    if (status){
-      console.log('status: ' + status)
-    }
-    if (status === 204){
-      this.getDrink(drinkId);
-    }
-  }
-
-  async patchDrink (endpoint, data, drinkId) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    const options = {
-      method: 'PATCH',
-      headers,
-      body: JSON.stringify(data)
-    };
-
-    const request = new Request(endpoint, options);
-    const response = await fetch(request);
-    const status = await response.status;
-
-    if (status === 202){
-      this.getDrinksAndGetDrink(drinkId);
-    }
-  }
-
-  async postIngredient (endpoint, data, drinkId) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    const options = {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(data)
-    };
-
-    const request = new Request(endpoint, options);
-    const response = await fetch(request);
-    const status = await response.status;
-
-    if (status === 201){
-      this.getDrink(drinkId);
-    }
-  }
-
-  getDrinks (id = -1) {
-    this.fetch('api/drinks')
-      .then(drinks => {
-        this.setState({drinks: drinks})
-        if (!this.state.drink){
-          this.getDrink(drinks[0].id)
-        }
-        if (id !== -1){
-          this.getDrink(drinks[0].id)
-        }
-      })
-  }
-
-  getDrink (id) {
-    this.fetch(`api/drinks/${id}`)
-      .then(drink => this.setState({drink: drink}))
-  }
-
-  getDrinksAndGetDrink (id) {
-    this.getDrinks();
-    this.getDrink(id);
-  }
-
-  newDrink () {
-    this.setState({drink: null})
-  }
-
-  flipButtonsDisabled () {
-    this.setState(this.state.buttonsDisabled ? {buttonsDisabled:false} : {buttonsDisabled:true})
-  }
-
-  render () {
-    let {drinks, drink, buttonsDisabled} = this.state
-    return drinks
-    ? <Container text>
-        <Header as='h2' icon textAlign='center'>
-          <Icon name='cocktail' circular />
-          <Header.Content>
-            List of Ingredients
-          </Header.Content>
-        </Header>
-
-        {
-          !buttonsDisabled &&
-          <Button onClick={() => this.newDrink()}>
-            New Drink
-          </Button>
-        }
-        {
-          buttonsDisabled &&
-          <Button disabled onClick={() => this.newDrink()}>
-            New Drink
-          </Button>
-        }
-
-        <Button.Group fluid widths={drinks.length}>
-          {
-            !buttonsDisabled &&
-            Object.keys(drinks).map((key) => {
-              return (
-                <Button active={drink && drink.id === drinks[key].id} fluid key={key} onClick={() => this.getDrink(drinks[key].id)}>
-                  {drinks[key].title}
-                </Button>
-              );
-            })
-          }
-          {
-            buttonsDisabled &&
-            Object.keys(drinks).map((key) => {
-              return (
-                <Button active={drink && drink.id === drinks[key].id} fluid disabled key={key} onClick={() => this.getDrink(drinks[key].id)}>
-                  {drinks[key].title}
-                </Button>
-              );
-            })
-          }
-        </Button.Group>
-
-        <Divider hidden />
-
-        {!drink &&
-          <DrinkForm
-            post={this.post}
-            getDrinks={this.getDrinks}
-          />
-        }
-
-        {drink &&
-          <Drink
-            title={this.state.drink.title}
-            description={this.state.drink.description}
-            ingredients={this.state.drink.ingredients}
-            steps={this.state.drink.steps}
-            source={this.state.drink.source}
-            drinkId={this.state.drink.id}
-            postIngredient={this.postIngredient}
-            flipButtonsDisabled={this.flipButtonsDisabled}
-            patchDrink={this.patchDrink}
-            buttonsDisabled={this.state.buttonsDisabled}
-            delete={this.delete}
-            deleteIngredient={this.deleteIngredient}
-          />
-        }
-      </Container>
-    : <Container text>
-        <Dimmer active inverted>
-          <Loader content='Loading' />
-        </Dimmer>
-      </Container>
-  }
 }
 
 export default App
---
+++ client/src/components/Head.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Header, Icon } from 'semantic-ui-react'
+
+class Head extends Component {
+  constructor (props) {
+    super(props)
+    this.state= {
+    }
+  }
+
+  render () {
+    return (
+  <Header as='h2' icon textAlign='center'>
+    <Icon name='cocktail' circular />
+    <Header.Content>
+      List of Ingredients
+    </Header.Content>
+  </Header>
+    )
+  }
+}
+
+export default Head
