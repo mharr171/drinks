fce3617f1d93930795927f0ec0efa0ac3e0665bb
Matthew Harris <mharr171.z@gmail.com>
2018-02-23 04:20:47 -0800

    create busy state for EditDrink

--- client/src/components/EditDrink.jsx
+++ client/src/components/EditDrink.jsx
@@ -12,17 +12,23 @@ class EditDrink extends Component {
       editingTitle: false,
       editingSource: false,
       editingDescription: false,
-      editingSteps: false
+      editingSteps: false,
+      busy: false
     }
 
     this.flip_editingTitle = this.flip_editingTitle.bind(this)
+    this.try_editTitle = this.try_editTitle.bind(this)
     this.flip_editingSource = this.flip_editingSource.bind(this)
+    this.try_editSource = this.try_editSource.bind(this)
     this.flip_editingDescription = this.flip_editingDescription.bind(this)
+    this.try_editDescription = this.try_editDescription.bind(this)
     this.flip_editingSteps = this.flip_editingSteps.bind(this)
+    this.try_editSteps = this.try_editSteps.bind(this)
+    this.flip_busy = this.flip_busy.bind(this)
   }
 
   render () {
-    let {editingTitle, editingSource, editingDescription, editingSteps} = this.state
+    let {editingTitle, editingSource, editingDescription, editingSteps, busy} = this.state
     // Variables
     let {drinkId, title, source, description, ingredients, steps} = this.props
     // Functions
@@ -32,7 +38,7 @@ class EditDrink extends Component {
 
           {
             title && !editingTitle &&
-            <div className="ui segment" onClick={this.flip_editingTitle}>
+            <div className="ui segment" onClick={this.try_editTitle}>
               <h5>Drink Name</h5>
               <h1>{title}</h1>
             </div>
@@ -53,7 +59,7 @@ class EditDrink extends Component {
 
           {
             source && !editingSource &&
-            <div className="ui segment" onClick={this.flip_editingSource}>
+            <div className="ui segment" onClick={this.try_editSource}>
               <h5>Source</h5>
               <p>{source}</p>
             </div>
@@ -74,7 +80,7 @@ class EditDrink extends Component {
 
           {
             description && !editingDescription &&
-            <div className="ui segment" onClick={this.flip_editingDescription}>
+            <div className="ui segment" onClick={this.try_editDescription}>
               <h5>Description</h5>
               <p>{description}</p>
             </div>
@@ -101,7 +107,7 @@ class EditDrink extends Component {
 
           {
             steps && !editingSteps &&
-            <div className="ui segment" onClick={this.flip_editingSteps}>
+            <div className="ui segment" onClick={this.try_editSteps}>
               <h5>Steps</h5>
               <p>{steps}</p>
             </div>
@@ -126,19 +132,50 @@ class EditDrink extends Component {
 
   flip_editingTitle (){
     this.state.editingTitle ? this.setState({editingTitle:false}) : this.setState({editingTitle:true})
+    this.flip_busy()
     this.props.flip_editFormIsOpen()
   }
+  try_editTitle (){
+    if (!this.state.busy){
+      this.flip_editingTitle()
+    }
+  }
+
   flip_editingSource (){
     this.state.editingSource ? this.setState({editingSource:false}) : this.setState({editingSource:true})
+    this.flip_busy()
     this.props.flip_editFormIsOpen()
   }
+  try_editSource (){
+    if (!this.state.busy){
+      this.flip_editingSource()
+    }
+  }
+
   flip_editingDescription (){
     this.state.editingDescription ? this.setState({editingDescription:false}) : this.setState({editingDescription:true})
+    this.flip_busy()
     this.props.flip_editFormIsOpen()
   }
+  try_editDescription (){
+    if (!this.state.busy){
+      this.flip_editingDescription()
+    }
+  }
+
   flip_editingSteps (){
     this.state.editingSteps ? this.setState({editingSteps:false}) : this.setState({editingSteps:true})
+    this.flip_busy()
     this.props.flip_editFormIsOpen()
+  }
+  try_editSteps (){
+    if (!this.state.busy){
+      this.flip_editingSteps()
+    }
+  }
+
+  flip_busy (){
+    this.state.busy ? this.setState({busy:false}) : this.setState({busy:true})
   }
 }
 
